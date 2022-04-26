9e7e3adde1857d6d2d1d5c58887b79e7b995c795
2447146432 <2447146432@qq.com>
2022-04-26 10:52:02 +0800

    每日学习

---
+++ train4/train4/test4.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//转义字符--转变原来的意思
+#include<stdio.h>
+void main()
+{
+	/*printf("c\tburvan\ttest\n");
+	printf("c\\tburvan\\ttest\n");
+	printf("%c\n", '\'');
+	printf("%c\n", '\"');*/
+	printf("%c\n",'\150');//\ddd--d表示8进制数
+	//8进制的150是十进制的104--对应ASCII码为h
+	printf("%c\n", '\x50');//\xdd--d表示16进制数
+	//16进制的50是10进制的80--对应ASCII码表P
+	return 0;
+}

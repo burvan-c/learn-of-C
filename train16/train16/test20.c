cb8bbd0fc985b0cf96af5603bf8002c6042569fc
2447146432 <2447146432@qq.com>
2022-05-26 16:47:42 +0800

    练习

---
+++ train16/train16/test20.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<string.h>
+#include<stdio.h>
+
+
+int main()
+{
+	//库函数
+	//char arr1[20] = { 0 };
+	//char arr2[] = "hello,mother fuck!";
+	//strcpy(arr1, arr2);//(destination,source)
+	//	printf("%s", arr1);
+
+
+	//memset--内存设置
+	/*char arr[] = "hello,mother fuck!";
+	memset(arr, 'x', 5);
+	printf("%s\n", arr);*/
+
+
+
+	//自定义函数
+	return 0;
+}

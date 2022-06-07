ee44b7e437b9d8f183aed4cfd4c3409dbc29d954
2447146432 <2447146432@qq.com>
2022-06-07 16:48:38 +0800

    learning

--- train16/train16/test24.c
+++ train16/train16/test24.c
@@ -54,18 +54,100 @@
 
 
 //输出乘法口诀表
-#include<stdio.h>
-int main()
-{
-	int i = 1;
-	for (i = 1; i <= 9; i++)
-	{
-		int j = 0;
-		for (j = 1; j <= i; j++)
-		{
-			printf("%d*%d=%-2d ", i, j, i * j);
-		}
-		printf("\n");
-	}
-	return 0;
-}
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-2d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//void print_table(int n)
+//{
+//	int i = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-2d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d",&n);
+//	print_table(n);
+//	return 0;
+//}
+
+//反向排列字符串
+//#include<stdio.h>
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+////void reverse_string(char* str)
+////{
+////	int left = 0;
+////	int right = my_strlen(str) - 1;
+////	while (left < right)
+////	{
+////		char tmp = str[left];
+////		str[left] = str[right];
+////		str[right] = tmp;
+////		left++;
+////		right--;
+////	}
+////}
+////void reverse_string(char* str)
+////{
+////	int left = 0;
+////	int right = my_strlen(str) - 1;
+////	while (left < right)
+////	{
+////		char tmp = *(str+left);
+////		*(str+left) = *(str+right);
+////		*(str+right) = tmp;
+////		left++;
+////		right--;
+////	}
+////}
+//void reverse_string(char* str)
+//{
+//		char tmp = *str;
+//		int len = my_strlen(str);
+//		*str = *(str + len - 1);
+//		*(str + len - 1) = '\0';
+//		if (my_strlen(str + 1) >= 2) 
+//		{
+//			reverse_string(str + 1);
+//		}
+//		*(str + len - 1) = tmp;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+

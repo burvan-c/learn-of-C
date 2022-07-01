178890246dee69239aa519f36750fd196febe1cd
2447146432 <2447146432@qq.com>
2022-07-01 18:19:20 +0800

    learning

--- train17/train17/test34.c
+++ train17/train17/test34.c
@@ -86,13 +86,67 @@
 
 
 
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//		//无符号数恒大于等于0---死循环
+//	}
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	char a[1000];
+//	int i;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//		//-1 -2……-128 127 126……3 2 1 0 -1 -2……-128 127 126……（圆圈循环）
+//		//原码 反码 补码    计算机以补码形式储存数据，以原码形式打印数据
+//	}
+//	printf("%d\n", strlen(a));//找到\0->0
+//	return 0;
+//}
+
+
+
+//unsigned char i = 0;
+//
+//int main()
+//{
+//	for (i = 0; i <= 255; i++)
+//	{
+//		printf("hello world\n");
+//		//死循环--无符号char范围：0-255；条件永远满足
+//
+//		return 0;
+//	}
+//}
+
+
+
+
+
+
+//浮点储存
+//limits.h定义整形家族（int  short  char)取值范围
+//float.h定义浮点型家族（float  double)取值范围
+
 int main()
 {
-	unsigned int i;
-	for (i = 9; i >= 0; i--)
-	{
-		printf("%u\n", i);
-		//无符号数恒大于等于0---死循环
-	}
+	int n = 9;
+	float* pfloat = (float*)&n;
+	printf("%d\n", n);
+	printf("%f\n", *pfloat);
+
+	*pfloat = 9.0;
+	printf("%d\n", n);
+	printf("%f\n", *pfloat);
 	return 0;
 }

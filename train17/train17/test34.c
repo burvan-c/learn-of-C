0f5b7416b525daa1966506b931eed8866e73d6ff
2447146432 <2447146432@qq.com>
2022-07-03 14:37:05 +0800

    learning

--- train17/train17/test34.c
+++ train17/train17/test34.c
@@ -1,5 +1,5 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
 
 //大端字节序：数据的低位字节序内容存放在高地址处，高位字节序的内容放在低地址处（由高到低）
 //小端字节序：数据的低位字节序内容存放在低地址处，高位字节序的内容放在高地址处 （由低到高）
@@ -138,18 +138,18 @@
 //limits.h定义整形家族（int  short  char)取值范围
 //float.h定义浮点型家族（float  double)取值范围
 
-int main()
-{
-	int n = 9;    //4byte
-	float* pfloat = (float*)&n;
-	printf("%d\n", n);
-	printf("%f\n", *pfloat);//4byte,以浮点数视角去看
-
-	*pfloat = 9.0;
-	printf("%d\n", n);
-	printf("%f\n", *pfloat);
-	return 0;
-}
+//int main()
+//{
+//	int n = 9;    //4byte
+//	float* pfloat = (float*)&n;
+//	printf("%d\n", n);
+//	printf("%f\n", *pfloat);//4byte,以浮点数视角去看 0.000……1001*2^-126
+//
+//	*pfloat = 9.0;//
+//	printf("%d\n", n);//S=0;E=3+127再转为二进制为10000010 M为00100……
+//	printf("%f\n", *pfloat);
+//	return 0;
+//}
 
 
 
@@ -159,10 +159,16 @@ int main()
 //M表示有效数字，大于等于1，小于2
 //2^E表示指数位
 
-//5.5的二进制为101.1->1.011*2^2->(-1)^0*1.011*2^2
-int main()
-{
-	float f = 5.5f;
 
-	return 0;
-}
+//E不全为0或不全为1，E为计算值减去127或1023
+//E全为0时，真实值为1-127或1-1023
+//E全为1时，若M全为0，表示无穷大
+
+
+//5.5的二进制为101.1->1.011*2^2->(-1)^0*1.011*2^2
+//int main()
+//{
+//	float f = 5.5f;
+//	
+//	return 0;
+//}
